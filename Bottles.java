63b9ae7df3cc46e78dc65ee10429f6cb123526b2
Pallavi5127 <pallavi02.xworkz@gmail.com>
2022-11-23 07:13:06 -0800

    commiting java files

---
+++ Bottles.java
@@ -0,0 +1,8 @@
+class Bottles{
+	public static void main(String bottles []){
+		String bottlesName = "Water Bottles";
+	
+	
+	System.out.println("Deffrent types  of Bottles are ---" + bottlesName);
+	}
+}
